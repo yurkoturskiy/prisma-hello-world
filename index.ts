f106e657e9683b2e02900054371c130b79c680b6
Yurko Turskiy <guandjoy@gmail.com>
2020-01-27 22:57:21 +0200

    Change database model

--- index.ts
+++ index.ts
@@ -2,13 +2,9 @@ import { prisma } from "./generated/prisma-client";
 
 // A `main` function so that we can use async/await
 async function main() {
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: "Alice" });
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
-
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users();
-  console.log(allUsers);
+  // Read the previously created user from the database and print their posts to the console
+  const postsByUser = await prisma.user({ email: "bob@prisma.io" }).posts();
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`);
 }
 
 main().catch(e => console.error(e));
